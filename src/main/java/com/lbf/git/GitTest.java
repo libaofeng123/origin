6e4843dc1f37ca1b78e66522fe7f7d9fb369c468
lbf <894831195@qq.com>
2023-04-03 23:47:23 +0800

    second commit

--- src/main/java/com/lbf/git/GitTest.java
+++ src/main/java/com/lbf/git/GitTest.java
@@ -6,6 +6,7 @@ package com.lbf.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello git!");
+        System.out.println("Hello git1!");
+        System.out.println("Hello git2!");
     }
 }
