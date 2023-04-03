ca48bf8b4cf5f4f278dda52b05fa69ffb6380bf4
lbf <894831195@qq.com>
2023-04-03 23:45:50 +0800

    first commit

---
+++ src/main/java/com/lbf/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lbf.git;
+
+/**
+ * @Author：lbaofeng
+ * @Date：2023/4/3 23:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
