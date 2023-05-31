01ad5721d27c64f7b1314bc97ad0418422fec4bd
lbaofeng <lbaofeng@linewell.com>
2023-05-31 17:22:53 +0800

    测试

--- src/main/java/com/lbf/git/GitTest.java
+++ src/main/java/com/lbf/git/GitTest.java
@@ -12,6 +12,6 @@ public class GitTest {
         System.out.println("Hello git4!");
         System.out.println("hot-fix commit2");
         System.out.println("hot-fix commit3");
-        System.out.println("hot-fix commit6");
+        System.out.println("hot-fix commit7");
     }
 }
